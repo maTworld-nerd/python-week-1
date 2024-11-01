e5785080f8929dadfbc9d6c597251ed70ec83d47
maTworld-nerd <hakicha69@gmail.com>
2024-11-01 14:45:28 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name =input('What is your name?')
+age = input("what's your age?")
+location = input("what's your location?")
+
+print("Hello", name.title(),"you are " + age, "years old, and your location is ", location.title())
