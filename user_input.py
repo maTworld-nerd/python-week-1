75ba08997c27785ed1801fe41486cde0ca69ce1a
maTworld-nerd <hakicha69@gmail.com>
2024-11-01 14:54:22 +0300

    done

--- user_input.py
+++ user_input.py
@@ -2,4 +2,4 @@ name =input('What is your name?')
 age = input("what's your age?")
 location = input("what's your location?")
 
-print("Hello", name.title(),"you are " + age, "years old, and your location is ", location.title())
+print("Hello", name.title(),"you are " + age, "years old, and you live in", location.title())
